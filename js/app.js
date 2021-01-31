47972fa07846fb01a1729495c8a455f3d0e139b4
HumbertoGlez <hglez.98s@gmail.com>
2021-01-30 19:49:37 -0600

    Se agrego movimiento, rotacion, seguimiento de camara y meta completando la escena

--- js/app.js
+++ js/app.js
@@ -2,16 +2,11 @@ import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 import { OBJLoader } from 'https://unpkg.com/three/examples/jsm/loaders/OBJLoader.js';
 import { GLTFLoader } from 'https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js';
 
-let windowHalfX = window.innerWidth / 2;
-let windowHalfY = window.innerHeight / 2;
-let mouseX = 0, mouseY = 0;
-
-// document.addEventListener('mousemove', onDocumentMouseMove, false );
+let clock = new THREE.Clock();
+let keyboard = new THREEx.KeyboardState();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(30, 3, 0);
-// camera.position.set(0, 0, 15);
-// camera.position.set(10, 7, 0);
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -27,7 +22,8 @@ let Kirby;
 var loader = new GLTFLoader();
 loader.load('../models/scene.gltf', function (gltf) {
     Kirby = gltf.scene;
-    Kirby.rotation.set(0, Math.PI, 0);
+    // Kirby.rotation.set(0, Math.PI, 0);
+    Kirby.rotateOnAxis( new THREE.Vector3(0, 1, 0), Math.PI);
     Kirby.position.set(23.5, 0, 0);
     scene.add(Kirby);
 
@@ -42,10 +38,11 @@ let kirbyFaceLight = new THREE.PointLight(0xfff07f, 1, 10)
 scene.add(kirbyFaceLight);
 
 // Set up a really dim and smooth blueish ambient light (its nightime)
-const ambientLight = new THREE.AmbientLight(0x0055a5, 0.1);
-scene.add(ambientLight);
+// const ambientLight = new THREE.AmbientLight(0x0055a5, 0.1);
+// scene.add(ambientLight);
 // Set up the biggest light source which is the moonlight
-var moonLight = new THREE.DirectionalLight(0xd9dee0, 0.4);
+// var moonLight = new THREE.DirectionalLight(0xd9dee0, 0.4);
+var moonLight = new THREE.DirectionalLight(0x0055a5, 0.6);
 moonLight.position.set(0, 25, 0).normalize();
 scene.add(moonLight);
 
@@ -56,7 +53,7 @@ moon.position.set(0, 15, 0);
 scene.add(moon);
 
 // Create ground
-const groundSize = 50;
+const groundSize = 60;
 const groundGeometry = new THREE.PlaneGeometry(groundSize, groundSize);
 var textureLoader = new THREE.TextureLoader();
 var groundTexture = textureLoader.load("../models/textures/stone_path.jpg");
@@ -65,8 +62,10 @@ groundTexture.repeat.set(groundSize, groundSize);
 const ground = new THREE.Mesh(groundGeometry, new THREE.MeshLambertMaterial({ map: groundTexture, side: THREE.DoubleSide }));
 ground.position.set(0, -0.01, 0);
 ground.rotation.set(Math.PI / 2, 0, 0);
+ground.matrixAutoUpdate = false;
 scene.add(ground);
-document.addEventListener('keydown', onDocumentKeyDown, false);
+ground.updateMatrix();
+
 // Create walls
 var walls = [];
 var wallHeight = 3;
@@ -89,6 +88,7 @@ walls.push(makeWall(1, wallHeight, 8, new THREE.Vector3(5, wallHeight / 2, 1)));
 walls.push(makeWall(1, wallHeight, 11, new THREE.Vector3(-13.5, wallHeight / 2, -8.5)));
 for (let i = 0; i < walls.length; i++) {
     scene.add(walls[i]);
+    walls[i].updateMatrix();
 }
 
 
@@ -121,44 +121,81 @@ function makeWall(width, height, depth, position) {
     var wall = new THREE.Mesh(wallGeometry, materials);
 
     wall.position.set(position.x, position.y, position.z);
+    wall.matrixAutoUpdate = false;
     return wall;
 }
 
+// instantiate a loader
+var loader = new OBJLoader();
+// load a resource
+loader.load(
+	// resource URL
+	'../models/Archway.obj',
+	// called when resource is loaded
+	function ( object ) {
+        object.material = new THREE.MeshLambertMaterial({ color: 0xFFb6c1 })
+        object.position.set(-38, 5, 17);
+        object.rotateOnAxis( new THREE.Vector3(0, 1, 0), Math.PI / 2 );
+		scene.add( object );
+
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
 const animate = function () {
     requestAnimationFrame(animate);
+    movement();
     render();
 };
 
-// function onDocumentMouseMove( event ) {
-//     mouseX = ( event.clientX - windowHalfX ) / 2;
-//     mouseY = ( event.clientY - windowHalfY ) / 2;
-// }
-
-function onDocumentKeyDown(event) {
+function movement() {
     if (Kirby) {
-        var speed = 0.1
-        if (event.keyCode == 87) {
-            Kirby.position.z -= speed;
-        } else if (event.keyCode == 83) {
-            Kirby.position.z -= speed;
-        } else if (event.keyCode == 65) {
-            Kirby.position.x -= speed;
-        } else if (event.keyCode == 68) {
-            Kirby.position.x += speed;
+        let delta = clock.getDelta(); // segundos
+        let rotateAngle = Math.PI / 2 * delta; // 200 pixeles por segundo
+        let moveDistance = 6 * delta;
+        
+        // W Hacia delante
+        if ( keyboard.pressed("W") ) {
+            Kirby.translateX( moveDistance );
         }
-        render();
+        // S Hacia atras
+        if ( keyboard.pressed("S") ) {
+            Kirby.translateX( -moveDistance );
+        }
+        // A Girar a la Izquierda
+        if ( keyboard.pressed("A") ) {
+            Kirby.rotateOnAxis( new THREE.Vector3(0, 1, 0), rotateAngle);
+        }
+        // D Girar a la Derecha
+        if ( keyboard.pressed("D") ) {
+            Kirby.rotateOnAxis( new THREE.Vector3(0, 1, 0), -rotateAngle);
+        }
     }
 };
 
 function render() {
-    // camera.position.x += ( mouseX - camera.position.x ) * .05;
-    // camera.position.y += ( - mouseY - camera.position.y ) * .05;
     if (kirbyFaceLight && Kirby) {
-        // console.log("setKirboLight");
         kirbyFaceLight.position.set(Kirby.position.x, Kirby.position.y + 1, Kirby.position.z);
         kirbyBottomLight.position.set(Kirby.position.x, Kirby.position.y - 1, Kirby.position.z);
+        let relativeCameraOffset = new THREE.Vector3(-10, 5, 0);
+        let cameraOffset = relativeCameraOffset.applyMatrix4( Kirby.matrixWorld );
+        camera.position.x = cameraOffset.x;
+        camera.position.y = cameraOffset.y;
+        camera.position.z = cameraOffset.z;
         camera.lookAt(new THREE.Vector3(Kirby.position.x, Kirby.position.y + 2, Kirby.position.z));
     } else {
+        camera.position.set(30, 5, 0);
         camera.lookAt(scene.position);
     }
     renderer.render(scene, camera);
